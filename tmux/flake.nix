fbfe194935a2c1206d6b2197e86e3fe026fa16fd
Sandro Padin <spadin@netflix.com>
2024-03-12 18:26:50 -0700

    commit

--- tmux/flake.nix
+++ tmux/flake.nix
@@ -3,35 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
-
-  outputs = { nixpkgs, flake-parts, self, ... } @ inputs:
-  flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-
-    perSystem = { system, self', ... }: let
-      pkgs = import nixpkgs {
-        inherit system;
-
-        config = {
-          programs.tmux = {
-            shortcut = "a";
-          };
-        };
-      };
-
-      tmux = pkgs.tmux;
-    in
-    {
-      packages = {
-        default = tmux;
-      };
+    flake-utils.url = "github:numtide/flake-utils";
+    tmux-gruvbox = {
+      url = "github:egel/tmux-gruvbox";
+      flake = false;
     };
   };
+
+  outputs = { nixpkgs, flake-utils, tmux-gruvbox, ... } @ inputs:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs { inherit system; };
+    tmux-gruvbox-light-conf = builtins.readFile ''${tmux-gruvbox}/tmux-gruvbox-light.conf'';
+    local-tmux-conf = builtins.readFile ./tmux.conf;
+
+    tmux-conf = pkgs.writeText "tmux.conf" ''
+      ${local-tmux-conf}
+      ${tmux-gruvbox-light-conf}
+    '';
+
+    tmux = with pkgs; symlinkJoin {
+      name = "tmux";
+      buildInputs = [ makeWrapper ];
+      paths = pkgs.tmux;
+      postBuild = ''
+        wrapProgram "$out/bin/tmux" --add-flags "-f ${tmux-conf}"
+      '';
+    };
+  in
+  {
+    packages = {
+      default = tmux;
+    };
+
+    devShells.default = pkgs.mkShell {
+      name = "tmux shell";
+      packages = with pkgs; [ tmux ];
+
+      # shellHook = ''
+      #   alias tmux="tmux -f ${tmux-conf}/etc/tmux.conf"
+      # '';
+    };
+  });
 }
