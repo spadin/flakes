85817ffee821162eda93b9f9c2b7f3f3d72db68c
Sandro Padin <spadin@netflix.com>
2024-03-12 21:29:43 -0700

    add hello

--- hello/flake.nix
+++ hello/flake.nix
@@ -7,9 +7,9 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.aarch64-darwin.hello = nixpkgs.legacyPackages.aarch64-darwin.hello;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    packages.aarch64-darwin.default = self.packages.aarch64-darwin.hello;
 
   };
 }
