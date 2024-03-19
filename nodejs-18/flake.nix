d98caa35c3ad47fd2e2de5389fb501b988aafb9b
Sandro Padin <spadin@netflix.com>
2024-03-19 11:35:07 -0700

    use most recent node 18

--- nodejs-18/flake.nix
+++ nodejs-18/flake.nix
@@ -10,17 +10,7 @@
   flake-utils.lib.eachDefaultSystem (system: 
   let
     pkgs = import nixpkgs { inherit system; };
-    old-pkgs = import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz";
-      sha256 = "1r6c7ggdk0546wzf2hvd5a7jwzsf3gn1flr8vjd685rm74syxv6d"; 
-    }) {
-      inherit system;
-
-      config = {
-        permittedInsecurePackages = [ "nodejs-18.17.1" ];
-      };
-    };
-    nodejs = old-pkgs.nodejs_18;
+    nodejs = pkgs.nodejs_18;
   in {
     packages = {
       default = nodejs;
